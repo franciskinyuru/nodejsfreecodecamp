5606d46c1842961254c8ea4ceaafc11337b28bc9
francis kinyuru <franciskinyuru26@gmail.com>
2023-01-11 17:00:28 +0300

    fs async and sync

---
+++ 11-fs-aync.js
@@ -0,0 +1,23 @@
+const {readFile, writeFile} = require('fs')
+readFile('./content/first.txt','utf8',(err, result)=>{
+    if(err){
+        console.log(err);
+    }
+    const first = result;
+    readFile('./content/second.txt','utf8',(err, result)=>{
+        if(err){
+            console.log(err);
+        }
+        const second = result;
+        writeFile(
+        './content/result-async.txt',
+        `Hello is the result ${first}, ${second}`,
+        (err, result)=>{
+            if (err) {
+                console.log(err);
+            }
+            console.log(result);
+        }
+    )
+    })
+})
--- app.js
+++
@@ -1,7 +0,0 @@
-const {readFile, writeFile} = require('fs')
-readFile('./content/first.txt','utf8',(err, result)=>{
-    if(err){
-        console.log(err);
-    }
-    console.log(result);
-})
