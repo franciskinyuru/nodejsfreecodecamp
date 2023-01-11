af669623fe20611c870a041eefe7777f099719ff
francis kinyuru <franciskinyuru26@gmail.com>
2023-01-11 15:54:26 +0300

    nodejs 101 freecodecamp

---
+++ 2globals.js
@@ -0,0 +1,7 @@
+// global variables
+console.log(__dirname);
+console.log(__filename);
+setInterval(() => {
+    console.log("Francis hI");
+}, 1000);
+console.log('ethiooia');
---
+++ 3names.js
@@ -0,0 +1,8 @@
+//modules
+//local
+const secret = "SUPER SECRET"
+// share
+const francis = "francis"
+const Imelda = 'Wambui'
+
+module.exports = {francis, Imelda}
