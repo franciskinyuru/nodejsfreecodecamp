7337f7c3d75bd536c7653444e4cb2efc40245d88
francis kinyuru <franciskinyuru26@gmail.com>
2023-01-12 11:10:09 +0300

    npm fo package mgmt

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-const { write } = require('fs');
-const http = require('http')
+// npm - global command, comes with node
+// npm --version
+// local dependency - use it only in this particular project
+// npm i <packageName>
 
-const server = http.createServer((req, res)=>{
-    if(req.url == '/'){
-        res.end('Welcome to our Home page')
-    }
-    if(req.url == "/about"){
-        res.end("Here is our short history")
-    }
+// global dependency - use it in any project
+// npm install -g <pkgName> for mac use sudo.
 
-    res.end(`<h1>Oops!</h1> <p>we can't fin the page you are looking for</p> <a href="/">Back home</a>`)
-    
-
-})
-const PORT = process.env.PORT || 5002;
-server.listen(PORT)
-//1:45:58 - let do it mu buddy
+const _ = require('lodash')
+const items = [1, [2, [3, [4]]]]
+const newItems = _.flattenDeep(items);
+console.log(newItems);
