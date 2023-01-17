12d9fe4e11f343672f422ea9803e2df9d0ecc91c
francis kinyuru <franciskinyuru26@gmail.com>
2023-01-17 16:37:14 +0300

    added put method

--- 2-express-tutorial/final/12-put.js
+++ 2-express-tutorial/final/12-put.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-let {people} = require('./data')
+let {people} = require('../data')
 
 //static assets
 app.use(express.static(path.join(__dirname+'/methods-public')))
+
 //parse form data
 app.use(express.urlencoded({extended: false}))
 //parse json 
@@ -54,18 +55,6 @@ app.put('/api/people/:id',(req, res)=>{
         return person
     })
     res.status(200).json({success:true, data: newPeople})
-})
-
-app.delete('/api/people/:id', (req, res)=>{
-    const {id} = req.params;
-    const person = people.find((person)=> person.id === Number(id))
-    if(!person){
-        return res.status(404).json({success:false, msg: `No person with id ${id}`}) 
-    }
-    const newPeople = people.filter((person)=>person.id !== Number(id))
-    return res.status(200).json({success:true, data: [newPeople]})
-    
-
 })
 
 app.listen(5001, ()=>{
