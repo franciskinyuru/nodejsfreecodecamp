ee1124f201fce9f7c3cabb81b62588c524fefdad
Francis kinyuru <franciskinyuru26@gmail.com>
2023-01-20 11:27:00 +0300

    Update connect.js

--- Task Manager/starter/db/connect.js
+++ Task Manager/starter/db/connect.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose')
-//const connectionString =''
-// pass = R0V3zcOgzxmZgK0S
 const connectDB = (url)=>{
 return mongoose
 .connect(url, {useNewUrlParser:true,useCreateIndex:true,useFindAndModify:false,useUnifiedTopology: true} )
