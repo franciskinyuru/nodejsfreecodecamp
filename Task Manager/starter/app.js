fc0fc03e1696c137897a30e5593d2235f0adfedb
francis kinyuru <franciskinyuru26@gmail.com>
2023-01-20 11:24:04 +0300

    added .env file for secrets management

--- Task Manager/starter/app.js
+++ Task Manager/starter/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
+require('dotenv').config()
 
 // middleware
 
@@ -17,10 +18,9 @@ app.use('/api/v1/tasks', tasks)
 app.use('/api/v1/tasks/:id', tasks)
 
 const port = 5003
-
 const start = async () =>{
     try {
-        await connectDB()
+        await connectDB(process.env.MONGO_URI)
         app.listen(port,()=>{
             console.log(`Server is running on port ${port}...`);
         })
@@ -29,6 +29,5 @@ const start = async () =>{
         
     }
 }
-
 start()
 
--- Task Manager/starter/db/connect.js
+++ Task Manager/starter/db/connect.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-const connectionString = 'mongodb+srv://waruikinyuru:R0V3zcOgzxmZgK0S@nodeepressproject.lhdzplz.mongodb.net/TASK_MANAGER?retryWrites=true&w=majority'
+//const connectionString =''
 // pass = R0V3zcOgzxmZgK0S
 const connectDB = (url)=>{
 return mongoose
-.connect(connectionString, {useNewUrlParser:true,useCreateIndex:true,useFindAndModify:false,useUnifiedTopology: true} )
+.connect(url, {useNewUrlParser:true,useCreateIndex:true,useFindAndModify:false,useUnifiedTopology: true} )
 }
 
 module.exports = connectDB
